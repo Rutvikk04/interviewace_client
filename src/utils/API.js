914ea799d7a00923aa155e5ab697cb9e23cbb415
rutvikk04 <r.amarcholi@gmail.com>
2025-10-05 11:41:57 +0530

    env updates iin apijs

--- src/utils/API.js
+++ src/utils/API.js
@@ -1,7 +1,7 @@
 import axios from "axios"
 import Cookies from "js-cookie"
 const API = axios.create({
-    baseURL: process.env.BACKEND_URL || "http://localhost:5000/"
+    baseURL: process.env.NEXT_PUBLIC_BACKEND_URL || "http://localhost:5000/"
 })
 
 API.interceptors.request.use(config => {
